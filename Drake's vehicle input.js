569f5dfdf5893cb58a0bbc676f550a998d3acf12
Draca Baugh-Flowers <66185683+Gradius64@users.noreply.github.com>
2024-11-29 17:38:13 -0600

    Add files via upload

---
+++ Drake's vehicle input.js
@@ -0,0 +1,15 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = ['Would you like to create a new vehicle or select an existing vehicle?', 'What type of vehicle would you like to create?,How muchis the vehicle?'];
+
+const question = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
